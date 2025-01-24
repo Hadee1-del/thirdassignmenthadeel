b28186a2d2e0b234e02e84efdeaf3e45742aa4bd
unknown <hadeelg592@gmial.com>
2025-01-24 21:14:55 +0300

    hadeelalkori3

--- lib/APIServices/DioClinetHadeel.dart
+++ lib/APIServices/DioClinetHadeel.dart
@@ -2,38 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:hadeelalkori3/Config/constants.dart';
 import 'package:hadeelalkori3/Controllers/LoginController.dart';
 import 'package:get/get.dart';
-import 'package:hadeelalkori3/Models/SubjectModels.dart';
-
-class ApiService {
-  final Dio dio = Dio();
-
-// إضافة مستخدم جديد
-Future<void> addUser(Courses user) async {
-  try {
-    await dio.post('https://jsonplaceholder.typicode.com/users', data: user.toJson());
-  } catch (e) {
-    throw Exception('Error adding user');
-  }
-}
-
-// تعديل بيانات مستخدم
-Future<void> updateUser(Courses user) async {
-  try {
-    await dio.put('https://jsonplaceholder.typicode.com/users/${user.id}', data: user.toJson());
-  } catch (e) {
-    throw Exception('Error updating user');
-  }
-}
-
-// حذف مستخدم
-Future<void> deleteUser(int id) async {
-  try {
-    await dio.delete('https://jsonplaceholder.typicode.com/users/$id');
-  } catch (e) {
-    throw Exception('Error deleting user');
-  }
-}
-}
 
 class DioClient {
   final Dio _dio = Dio(BaseOptions(
--- lib/Config/constants.dart
+++ lib/Config/constants.dart
@@ -2,7 +2,10 @@ const String baseURL="https://lomfu.pythonanywhere.com";
 const String baseAPIURLV1="/api/v1";
 const String loginAPI="/accounts/login/";
 const String refreshTokeAPI="/accounts/token/refresh/";
+
 const String subjectsAPI="/courses/subjects/";
-const String teachersAPIPost="/teachers/courses/create/";
-const String teahersAPIPut="/teachers/courses/{id}/update/";
-const String teachersAPIDelete="/teachers/courses/{id}/update/";
+const String teachersAPI="/teachers/courses/";
+const String addAPI="/create/";
+const String deleteAPI="/delete/";
+const String updateAPI="/update/";
+const String getAPI="/get/";
